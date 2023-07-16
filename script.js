addc6fbdc5527901122bde4a0e00a5421300c857
Sky_Thunder <107789489+Thunder-Sky@users.noreply.github.com>
2023-07-16 12:26:11 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-i=document.getElementsByClassName("text-center"),t=[];
-for(c=7;c<i.length;c++){
-    if(i[c].children[4].innerText!="-") t.push(i[c].children[4].innerText);
-}
-link = document.createElement('a');
-link.href = URL.createObjectURL(new Blob([t.reverse().join("\n")],{type:"text/plain"}));
-link.download = 'test.txt';
-link.click();
