8d7f670c2daca825805bd1cdb5256a3ec80fbade
Sky_Thunder <107789489+Thunder-Sky@users.noreply.github.com>
2023-07-16 15:58:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-async function access(){
-    var res = await fetch("https://script.google.com/macros/s/AKfycbyOEzub_UX-nEISIiwk2d1oFsBVKw6w5Sqg28g7k_l7_t3nVSeHz_x5PTb0f4pJk0vi/exec?name=Sky_Thunder");
+async function access(username){
+    var username = username;
+    var res = await fetch(`https://script.google.com/macros/s/AKfycbyOEzub_UX-nEISIiwk2d1oFsBVKw6w5Sqg28g7k_l7_t3nVSeHz_x5PTb0f4pJk0vi/exec?name=${username}`);
     var res = await res.json();
     link = document.createElement('a');
     link.href = URL.createObjectURL(new Blob([res.map(n=>n.Rating).join("\n")],{type:"text/plain"}));
@@ -11,4 +12,4 @@ async function access(){
     link.click();
 }
 
-access();
+access(); //　かっこの中にAtCoderのユーザー名を入れてください 例: access("Sky_Thunder");
